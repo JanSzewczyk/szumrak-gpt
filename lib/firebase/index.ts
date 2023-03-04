d22ebd283547b5e39247a221a9deb0a14760a52e
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-03-04 19:49:41 +0100

    refactor(envs): refactor firebase client envs

--- lib/firebase/index.ts
+++ lib/firebase/index.ts
@@ -2,13 +2,13 @@ import { FirebaseOptions, getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyDwTjJRsixUP6_zYlogdwhy0Mh8PQMV__M",
-  appId: "1:303485525928:web:5fc87bbfdf7ef45221c79d",
-  authDomain: "szumrak-gpt-344aa.firebaseapp.com",
-  measurementId: "G-XPTWP238ZT",
-  messagingSenderId: "303485525928",
-  projectId: "szumrak-gpt-344aa",
-  storageBucket: "szumrak-gpt-344aa.appspot.com"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET
 };
 
 // Initialize Firebase
